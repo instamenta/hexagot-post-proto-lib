28f1a72188960744980e5af374371ec6805d7ce0
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-08-14 22:17:48 +0300

    made exports v1.0.0 ready

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	messages: require('./generated/posts_pb'),
+	service: require('./generated/posts_grpc_pb'),
+	messages_type: require('./generated/types/posts_pb'),
+	service_type: require('./generated/types/posts_grpc_pb'),
+}
